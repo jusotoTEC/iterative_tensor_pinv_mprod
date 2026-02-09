35efcb6ad5b182d51ec42b62423c07b76d145d0e
jusotoTEC <jusoto@itcr.ac.cr>
2026-02-09 08:30:18 -0600

    file

--- Numerical Experiment 1/mode3_product.m
+++
@@ -1,6 +0,0 @@
-function D=mode3_product(C,M)
-    [m,n,p]=size(C);
-    C2 = reshape(C, [m*n, p])';       
-    D2 = M * C2;     
-    D = reshape(D2', [m, n, size(M,1)]);
-end 
--- Numerical Experiment 1/normFrob3d.m
+++
@@ -1,3 +0,0 @@
-function x=normFrob3d(A)
-    x = sqrt(sum(A.^2,'all'));
-end
